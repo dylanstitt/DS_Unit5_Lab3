64a15592f3401182f2c884905e357f7110fed8a2
Dylan Stitt <dstitt527@gmail.com>
2025-02-05 12:27:58 -0500

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+# Dylan Stitt
+# Unit 5 Lab 3
+# Reverse Linked Lists
+
+from SinglyLinkedList import *
+from DoublyLinkedList import *
+import os
+
+def reverseLL(LL):
+    if type(LL) == SinglyLinkedList:
+        ...
+    else:
+        ...
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
